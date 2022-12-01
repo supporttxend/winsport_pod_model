53fa97e27bb827ea0f7776ea75a329d4763b2300
awais.malik <awais.malik@thdinfinity.com>
2022-12-01 15:07:44 +0500

    modal training initial code

---
+++ main.py
@@ -0,0 +1,25 @@
+from re import T
+from fastapi import FastAPI
+import uvicorn
+import model_prdiction
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/data", StaticFiles(directory="data"), name="data")
+
+app.include_router(
+    model_prdiction.router, prefix="/model_prediction", tags=["model_prediction"]
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=7000, host="0.0.0.0", reload=True)
